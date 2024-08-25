d9f9d8457cd12917b4b157c5e9228ce0ab0ffc16
Ezeh20 <ranter1570@gmail.com>
2024-08-26 00:19:04 +0100

    ..

--- src/components/taskAction/TaskAction.vue
+++ src/components/taskAction/TaskAction.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts">
+<script setup>
 import { useToggleStatus } from '../../composables/toggleStatus';
 import styles from './TaskAction.module.css'
 import { defineProps } from "vue";
 import { TaskType } from '../../types';
 
 
-const props = defineProps<{
-    db: { tasks: TaskType[] }
-    id: number
-}>()
+const props = defineProps({
+    db: Object,
+    id: Number
+})
 
 const { db, id } = props;
 const { handleCompleteTask } = useToggleStatus(db, id)
